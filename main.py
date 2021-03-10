da8ccf34f6cda4a2623ed1a2207536929ad421f7
Utkarsh Varma <uvroxx2323@gmail.com>
2021-03-10 00:03:10 -0500

    Finalized the call function

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ from bs4 import BeautifulSoup
 import re
 from time import sleep
 
-
-
 url_head = "https://www.kijiji.ca"
 urls = []
 numbers = []
-total=0
-kijiji_head = "https://www.kijiji.ca/b-achat-et-vente/ville-de-montreal/wholesale/page-"
+total = 0
+kijiji_head = "https://www.kijiji.ca/b-achat-et-vente/ville-de-montreal/"
 kijiji_tail = "/k0c10l1700281?radius=20.0&address=Chinatown%2C+Ville-Marie%2C+Montreal%2C+QC&ll=45.507693,-73.560141&rb=true"
 
 HEADER = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
+
 def find_hitpoints(hitpoint):
-    while urls==[]:
+    while urls == []:
         global total
         response = requests.get(hitpoint, headers=HEADER)
         pprint(response)
@@ -25,11 +24,9 @@ def find_hitpoints(hitpoint):
         info = soup.find_all(class_="info-container")
         for data in info:
             urls.append(url_head + str(data.find(name="a")).split('href="')[1].strip().split('">')[0])
-        total=len(urls)
-        if urls==[]:
+        total = len(urls)
+        if urls == []:
             sleep(60)
-
-
 
 
 def extractNum(string):
@@ -46,7 +43,7 @@ def extractNum(string):
 
 
 def getNumber(url):
-    text="none"
+    text = "none"
     response = requests.get(url, headers=HEADER)
     soup = BeautifulSoup(response.text, "html.parser")
     try:
@@ -58,25 +55,28 @@ def getNumber(url):
     return (extractNum(text))
 
 
-def start_collecting(page_num):
-    hitpoint = kijiji_head+str(page_num)+kijiji_tail
+def start_collecting(keyword, page_num):
+    hitpoint = kijiji_head + "/page-" + str(page_num) + kijiji_tail
     find_hitpoints(hitpoint)
     pprint(urls)
     at = 0
     for url in urls:
         print(f"Im at {at}/{total}")
         num = getNumber(url)
-        at+=1
+        at += 1
         if len(num) > 9:
-            data={
-                "num":num,
-                "url":url
+            data = {
+                "num": num,
+                "url": url
             }
             numbers.append(data)
 
 
-while len(numbers)<30:
-    page_number=0
-    start_collecting(page_number)
-    page_number+=1
-    pprint(numbers)
+def findContacts(keyword, target):
+    while len(numbers) < target:
+        page_number = 0
+        start_collecting(keyword, page_number)
+        page_number += 1
+        pprint(numbers)
+
+findContacts("wholesale",10)
